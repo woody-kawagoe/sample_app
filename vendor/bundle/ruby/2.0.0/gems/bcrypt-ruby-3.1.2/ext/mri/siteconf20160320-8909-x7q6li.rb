d09b36572c522bae5f4fe5304ec01c9f234d0335
woody-kawagoe <hip.hop.zip.please@gmail.com>
2016-03-20 13:02:08 +0900

    create user signin page

---
+++ vendor/bundle/ruby/2.0.0/gems/bcrypt-ruby-3.1.2/ext/mri/siteconf20160320-8909-x7q6li.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20160320-8909-1h6u8e2"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
